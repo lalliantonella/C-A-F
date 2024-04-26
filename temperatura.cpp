4ec9ba61c5c283ccd20f4fbc2fa8356accfd4d6f
lalliantonella <lalliantonella03@gmail.com>
2024-04-26 12:22:50 -0300

    first commit

---
+++ temperatura.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+double C;
+double F;
+cout << "INGRESE UNA TEMPERATURA EN GRADOS CELSIUS ";
+cin >> C;
+             F = (C * 1.8) + 32;
+
+             cout << "LA TEMPERATURA ES " << F;
+return 0;
+}
